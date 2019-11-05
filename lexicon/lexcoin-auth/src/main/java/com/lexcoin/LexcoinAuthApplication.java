a1588e675a17339258d4bce79c23975ab751b9ef
chenyu <jchen_px@163.com>
2019-11-05 20:50:44 +0800

    添加nacos配置

--- lexicon/lexcoin-auth/src/main/java/com/lexcoin/LexcoinAuthApplication.java
+++ lexicon/lexcoin-auth/src/main/java/com/lexcoin/LexcoinAuthApplication.java
@@ -2,8 +2,12 @@ package com.lexcoin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan(basePackages="com.lexcoin.mapper")
 public class LexcoinAuthApplication {
 
     public static void main(String[] args) {
